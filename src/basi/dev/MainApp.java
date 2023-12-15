c79948249d1366d0c8893c309bcf2c2cd6847664
KyoDyy <144084641+LeQuangDuy0@users.noreply.github.com>
2023-12-16 01:57:24 +0700

    Add files via upload

---
+++ src/basi/dev/MainApp.java
@@ -0,0 +1,25 @@
+package basi.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		/**
+		 * Khai báo biến
+		 */
+		String firstName = "Phan van";
+		String lastName = "Nam";
+		int old = 15;
+		float markMath = 9f;
+		boolean passed = true;
+
+		System.out.println("Ho va ten");
+		System.out.println(firstName + " " + lastName);
+		System.out.println("Mark Math: " + markMath);
+		System.out.println("passed: " + passed);
+
+		// Khai báo hằng. Hằng phải viết hoa
+		final float PI = 3.14f;
+
+	}
+
+}
